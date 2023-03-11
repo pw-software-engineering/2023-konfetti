c9dd37446f9ade65e0cb5b035d5f3796dd132c5e
bjadczak <73365815+bjadczak@users.noreply.github.com>
2023-03-11 20:11:16 +0100

    Create tests that verify login and register pages

--- src/front/ticketer/test/widget_test.dart
+++ src/front/ticketer/test/widget_test.dart
@@ -1,30 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ticketer/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Change login to register form smoke test',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that we see a login page
+    expect(find.widgetWithText(ElevatedButton, "Login"), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, "Register"), findsNothing);
+    expect(find.widgetWithText(TextFormField, "e-mail"), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the "Register instead" button and trigger a frame.
+    await tester.tap(find.widgetWithText(TextButton, "Register instead"));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that we see a register page
+    expect(find.widgetWithText(ElevatedButton, "Login"), findsNothing);
+    expect(find.widgetWithText(ElevatedButton, "Register"), findsOneWidget);
+    expect(find.widgetWithText(TextFormField, "e-mail"), findsOneWidget);
   });
 }
