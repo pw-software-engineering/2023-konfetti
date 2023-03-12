18b203a4203cf258f41bbfd1ff29dcc706380e5c
Michal Boguszewski <michal1842001@gmail.com>
2023-03-12 17:51:33 +0100

    Register in memory db provider

--- src/back/TicketManager/src/Core/TicketManager.Core.Api/Program.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TicketManager.Core.Services.DataAccess;
+
 namespace TicketManager.Core.Api;
 
 public class Program
@@ -5,6 +8,11 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddDbContext<CoreDbContext>(
+            opts => opts.UseInMemoryDatabase("Database")
+        );
+        
         var app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
