9ab74b8c3bcbcaed2bb73251559038f3f63f75de
Michal Boguszewski <michal1842001@gmail.com>
2023-03-12 17:04:46 +0100

    Create tests for Account constructor

---
+++ src/back/TicketManager/tests/Core/TicketManager.Core.DomainTests/Accounts/AccountTests.cs
@@ -0,0 +1,26 @@
+using FluentAssertions;
+using TicketManager.Core.Domain.Accounts;
+using Xunit;
+
+namespace TicketManager.Core.DomainTests.Accounts;
+
+public class AccountTests
+{
+    public class ConstructorTests
+    {
+        [Theory]
+        [InlineData("617bf402-dd16-4e0d-8fd2-bc66429eee35", "email@email.com", "passwordHash")]
+        [InlineData("b41d4f5b-5465-44dd-ab4f-e8f215c9913b", "michal@gmail.com", "passwordHash2")]
+        [InlineData("e840230d-00ca-430a-9eb4-ab935346d34b", "kuba@email.com", "passwordHash3")]
+        [InlineData("98632145-9618-44d5-9623-71a3023bdd54", "lionel@messi.com", "passwordHash4")]
+        public void WhenValidParametersProvided_ItShouldSetThemAsProperties(string idAsString, string email, string passwordHash)
+        {
+            var id = new Guid(idAsString);
+            var account = new Account(id, email, passwordHash);
+
+            account.Id.Should().Be(id);
+            account.Email.Should().Be(email);
+            account.PasswordHash.Should().Be(passwordHash);
+        }
+    }
+}
