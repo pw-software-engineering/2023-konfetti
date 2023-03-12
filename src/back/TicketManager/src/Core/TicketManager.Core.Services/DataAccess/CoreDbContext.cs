90153286fb5ceecb7eacdc71153f5311fb67e30b
Michal Boguszewski <michal1842001@gmail.com>
2023-03-12 22:13:51 +0100

    Configure users and accounts

--- src/back/TicketManager/src/Core/TicketManager.Core.Services/DataAccess/CoreDbContext.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Services/DataAccess/CoreDbContext.cs
@@ -14,5 +14,28 @@ public class CoreDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        ConfigureUsers(modelBuilder);
+        ConfigureAccounts(modelBuilder);
+    }
+
+    private void ConfigureUsers(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(cfg =>
+        {
+            cfg.HasKey(e => e.Id);
+            cfg.Property(e => e.Email).HasMaxLength(StringLengths.MediumString);
+            cfg.Property(e => e.FirstName).HasMaxLength(StringLengths.ShortString);
+            cfg.Property(e => e.LastName).HasMaxLength(StringLengths.ShortString);
+        });
+    }
+    
+    private void ConfigureAccounts(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>(cfg =>
+        {
+            cfg.HasKey(e => e.Id);
+            cfg.Property(e => e.Email).HasMaxLength(StringLengths.MediumString);
+            cfg.Property(e => e.PasswordHash).HasMaxLength(StringLengths.MediumString);
+        });
     }
 }
