e2ec80a93b6731ce6bb62f36ba6fb403d86e3aac
Michal Boguszewski <michal1842001@gmail.com>
2023-03-14 16:00:49 +0100

    Add RegisterUser validation

--- src/back/TicketManager/src/Core/TicketManager.Core.Contracts/Users/RegisterUserRequest.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Contracts/Users/RegisterUserRequest.cs
@@ -7,4 +7,19 @@ public class RegisterUserRequest
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateOnly BirthDate { get; set; }
+
+    public static class ErrorCodes
+    {
+        public const int EmailIsEmpty = 1;
+        public const int EmailIsTooLong = 2;
+        public const int EmailIsAlreadyTaken = 3;
+        public const int EmailIsInvalid = 4;
+        public const int PasswordIsEmpty = 5;
+        public const int PasswordIsTooLong = 6;
+        public const int PasswordIsInvalid = 7;
+        public const int FirstNameIsEmpty = 8;
+        public const int FirstNameIsTooLong = 9;
+        public const int LastNameIsEmpty = 10;
+        public const int LastNameIsTooLong = 11;
+    }
 }
