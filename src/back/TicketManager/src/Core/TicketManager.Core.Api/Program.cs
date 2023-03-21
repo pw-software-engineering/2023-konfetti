994db926400871f61317659fc61f33bfbe4bd576
Birchman <jakubbrzoskowski@gmail.com>
2023-03-21 15:11:50 +0100

    Add JWT token in auth process

--- src/back/TicketManager/src/Core/TicketManager.Core.Api/Program.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Api/Program.cs
@@ -1,4 +1,5 @@
 using FastEndpoints;
+using FastEndpoints.Security;
 using FastEndpoints.Swagger;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -27,12 +28,14 @@ public class Program
                 .UseInMemoryDatabase("Database")
         );
 
+        var signingKey = "UWaNHq1sR+3HEYyrcqO1MLa4zgtR9mYHW/wRYNsBzKRlqBMUD8U3sLUS0+j2RsN2tfNV4rQhhxfcmNmDldk94EOtDiAxg8By6YUod0fXIgWGykeb7VYg5s/NzS1UTTe8Fj7ddB522HwR3iCz97sF3H2oUW0MFYtJr9eF61MG+ZHbaw4FWeqGwqc9W0is/Q4ceLzBR3ndS+gsT/5sdMVpAt+oVa0Z08WG0BCRJrFyJhcxOkC2UGGGQVxcGUHS/ICP5zgWcOp3/iDswC6MBkl3W1T4BFmGyrBhjArGWaCwo2ae0/Z0rvSkeERgF4+AMFNRIjAYEcERFUhG1kgwL1/vAw=="; // TODO: Move to variable
+
+        
         builder.Services.AddSingleton<PasswordManager>();
         builder.Services.AddSingleton<MockableCoreDbResolver>();
         builder.Services.AddSingleton<TokenCreator>();
-        builder.Services.AddSingleton<TokenConfiguration>(new TokenConfiguration(
-            "UWaNHq1sR+3HEYyrcqO1MLa4zgtR9mYHW/wRYNsBzKRlqBMUD8U3sLUS0+j2RsN2tfNV4rQhhxfcmNmDldk94EOtDiAxg8By6YUod0fXIgWGykeb7VYg5s/NzS1UTTe8Fj7ddB522HwR3iCz97sF3H2oUW0MFYtJr9eF61MG+ZHbaw4FWeqGwqc9W0is/Q4ceLzBR3ndS+gsT/5sdMVpAt+oVa0Z08WG0BCRJrFyJhcxOkC2UGGGQVxcGUHS/ICP5zgWcOp3/iDswC6MBkl3W1T4BFmGyrBhjArGWaCwo2ae0/Z0rvSkeERgF4+AMFNRIjAYEcERFUhG1kgwL1/vAw==")); // TODO: Move to variable
-        
+        builder.Services.AddSingleton<TokenConfiguration>(new TokenConfiguration(signingKey));
+            
         builder.Services.AddScoped<Repository<User, Guid>>();
         builder.Services.AddScoped<Repository<Organizer, Guid>>();
         builder.Services.AddScoped<Repository<Account, Guid>>();
@@ -46,6 +49,8 @@ public class Program
             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
             options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
         });
+
+        builder.Services.AddJWTBearerAuth(signingKey);
         
         var app = builder.Build();
 
