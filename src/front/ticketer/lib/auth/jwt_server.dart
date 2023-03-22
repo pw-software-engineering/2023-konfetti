cb6cde077c34a83fbfae285d8e9cf846063d4d83
bjadczak <73365815+bjadczak@users.noreply.github.com>
2023-03-22 18:12:33 +0100

    Add mocked server servis for tests

---
+++ src/front/ticketer/lib/auth/jwt_server.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+
+void main() async {
+  var app = Router();
+
+  app.post('/login', (Request request) {
+    var json = jsonEncode({
+      'access_token':
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    });
+
+    return Response.ok(json, headers: {'Access-Control-Allow-Origin': '*'});
+  });
+
+  await io.serve(app, 'localhost', 8080);
+}
