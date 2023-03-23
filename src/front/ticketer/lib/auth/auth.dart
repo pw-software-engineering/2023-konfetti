53c7a5e473dbc78ca6ae9f19c42da615a42cfb79
Jakub Górka <kubagorka27@gmail.com>
2023-03-23 01:53:02 +0100

    Fix failing tests

--- src/front/ticketer/lib/auth/auth.dart
+++ src/front/ticketer/lib/auth/auth.dart
@@ -10,7 +10,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:ticketer/model/credentials.dart';
 
 import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:ticketer/model/user_type.dart';
 
 class AuthModel extends ChangeNotifier {
   final storage = const FlutterSecureStorage();
