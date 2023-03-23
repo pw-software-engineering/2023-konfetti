c27567456d85772f887b0f097d8cf0a7b30dfe40
Michal Boguszewski <michal1842001@gmail.com>
2023-03-23 22:19:51 +0100

    Configure validation error response

---
+++ src/back/TicketManager/src/Core/TicketManager.Core.Contracts/Validation/ValidationErrorResponse.cs
@@ -0,0 +1,12 @@
+namespace TicketManager.Core.Contracts.Validation;
+
+public class ValidationErrorResponse
+{
+    public List<ValidationError> Errors { get; set; }
+}
+
+public class ValidationError
+{
+    public int ErrorCode { get; set; }
+    public string ErrorMessage { get; set; }
+}
