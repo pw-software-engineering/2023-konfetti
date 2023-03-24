a28420921a481908f8b8fd6004e73129992e0070
bjadczak <73365815+bjadczak@users.noreply.github.com>
2023-03-24 08:03:29 +0100

    Fix dotenv

--- src/front/ticketer/lib/main.dart
+++ src/front/ticketer/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "dotenv");
 
   runApp(const MyApp());
 }
