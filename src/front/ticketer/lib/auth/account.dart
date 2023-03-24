9c1e8d67ea598d1bf168c33aaa6ba67e698a8462
bjadczak <73365815+bjadczak@users.noreply.github.com>
2023-03-24 07:55:50 +0100

    Fix contructor of Account class

--- src/front/ticketer/lib/auth/account.dart
+++ src/front/ticketer/lib/auth/account.dart
@@ -4,7 +4,6 @@ class Account {
   late AccountType type;
 
   Account(String accountType) {
-    AccountType type = AccountType.values.firstWhere((e) => e.name == accountType);
-    type = type;
+    type = AccountType.values.firstWhere((e) => e.name == accountType);
   }
 }
