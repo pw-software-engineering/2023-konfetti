b9ad7268ff46af4322b50a10274f13445655e613
Michal Boguszewski <michal1842001@gmail.com>
2023-03-25 11:30:35 +0100

    Create CreateEventRequest

---
+++ src/back/TicketManager/src/Core/TicketManager.Core.Contracts/Events/CreateEventRequest.cs
@@ -0,0 +1,23 @@
+namespace TicketManager.Core.Contracts.Events;
+
+public class CreateEventRequest
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public DateTime Date { get; set; }
+    public List<SectorDto> Sectors { get; set; }
+
+    public static class ErrorCodes
+    {
+        public const int NameIsEmpty = 1;
+        public const int NameIsTooLong = 2;
+        public const int DescriptionIsEmpty = 3;
+        public const int DescriptionIsTooLong = 4;
+        public const int LocationIsEmpty = 5;
+        public const int LocationIsTooLong = 6;
+        public const int DateIsNotFuture = 7;
+
+        public class SectorErrorCodes : SectorDto.ErrorCodes { }
+    }
+}
