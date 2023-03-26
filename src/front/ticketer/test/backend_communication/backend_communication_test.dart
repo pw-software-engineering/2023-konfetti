860847c31a72cf2b8417f3a9c683c055d09043d7
bjadczak <73365815+bjadczak@users.noreply.github.com>
2023-03-26 19:06:37 +0200

    Add tests

---
+++ src/front/ticketer/test/backend_communication/backend_communication_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ticketer/backend_communication/logic/communication.dart';
+import 'package:ticketer/backend_communication/model/credentials.dart';
+import 'package:ticketer/backend_communication/model/organizer.dart';
+import 'package:ticketer/backend_communication/model/tax_type.dart';
+import 'package:ticketer/backend_communication/model/user.dart';
+
+void main() async {
+  group('BackendCommunication tests', () {
+    test('Without initialization BackendCommunication() should throw exception',
+        () {
+      assert(BackendCommunication().isInitialized == false);
+      var organizer = Organizer(
+          "companyName",
+          "address",
+          TaxType.NIP,
+          "123123123",
+          "displayName",
+          "email@email.com",
+          "password",
+          "123123123");
+      var user = User(
+          "firstName", "lastName", "2020-10-10", "email@email.com", "password");
+      expect(() => BackendCommunication().postCall("account/login"),
+          throwsA(anything));
+      expect(() => BackendCommunication().organizer.register(organizer),
+          throwsA(anything));
+      expect(() => BackendCommunication().organizer.update(organizer),
+          throwsA(anything));
+      expect(
+          () => BackendCommunication().user.register(user), throwsA(anything));
+      expect(() => BackendCommunication().user.update(user), throwsA(anything));
+      expect(
+          () => BackendCommunication()
+              .account
+              .login(Credentials("email@email.com", "password")),
+          throwsA(anything));
+    });
+  });
+}
