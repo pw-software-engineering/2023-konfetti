19f0783eddbcfa2994d03db81d9804a087bb7e25
Jakub Brzóskowski <43820607+jaBrzosko@users.noreply.github.com>
2023-03-28 11:41:10 +0200

    Add hardcoded admin user (#42)

---
+++ src/back/TicketManager/src/Core/TicketManager.Core.Services/Migrations/20230328092103_AddAdminAccount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketManager.Core.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO \"Accounts\" (\"Id\", \"Email\", \"PasswordHash\", \"Role\")" +
+                                 "VALUES ('bedebffa-73dc-4944-9727-4924d82dcbe2', 'admin@email.com', " +
+                                 "'iGwhjRGxH2FEwca2gtikmjJojdRH7Xa+jxweKxukYKFz/Bgx', 'Admin');");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
