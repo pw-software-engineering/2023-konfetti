9634ad42470d24cbea1d746275ff2d25b2ad3f84
Birchman <jakubbrzoskowski@gmail.com>
2023-03-30 10:23:07 +0200

    Add validator for organizer decide

--- src/back/TicketManager/src/Core/TicketManager.Core.Contracts/Organizers/OrganizerDecideRequest.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Contracts/Organizers/OrganizerDecideRequest.cs
@@ -4,4 +4,10 @@ public class OrganizerDecideRequest
 {
     public Guid OrganizerId { get; set; }
     public bool IsAccepted { get; set; }
+
+    public static class ErrorCodes
+    {
+        public static int OrganizerNotInDatabase = 1;
+        public static int OrganizerAlreadyVerified = 2;
+    }
 }
---
+++ src/back/TicketManager/src/Core/TicketManager.Core.Services/Endpoints/Organizers/OrganizerDecideValidator.cs
@@ -0,0 +1,46 @@
+using FastEndpoints;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TicketManager.Core.Contracts.Organizers;
+using TicketManager.Core.Domain.Organizer;
+using TicketManager.Core.Services.Services.Mockables;
+using TicketManager.Core.Services.ValidationExtensions;
+
+namespace TicketManager.Core.Services.Endpoints.Organizers;
+
+public class OrganizerDecideValidator : Validator<OrganizerDecideRequest>
+{
+    private IServiceScopeFactory scopeFactory;
+    private MockableCoreDbResolver dbResolver;
+    
+    public OrganizerDecideValidator(IServiceScopeFactory scopeFactory, MockableCoreDbResolver dbResolver)
+    {
+        this.scopeFactory = scopeFactory;
+        this.dbResolver = dbResolver;
+
+        RuleFor(req => req.OrganizerId)
+            .MustAsync(IsIdPresent)
+            .WithCode(OrganizerDecideRequest.ErrorCodes.OrganizerNotInDatabase)
+            .MustAsync(IsUnverified)
+            .WithCode(OrganizerDecideRequest.ErrorCodes.OrganizerAlreadyVerified);
+    }
+    
+    private async Task<bool> IsIdPresent(Guid id, CancellationToken cancellationToken)
+    {
+        using var scope = scopeFactory.CreateScope();
+        
+        return await dbResolver.Resolve(scope)
+            .Organizers
+            .AnyAsync(o => o.Id != id, cancellationToken);
+    }
+    
+    private async Task<bool> IsUnverified(Guid id, CancellationToken cancellationToken)
+    {
+        using var scope = scopeFactory.CreateScope();
+        
+        return await dbResolver.Resolve(scope)
+            .Organizers
+            .AnyAsync(o => o.Id == id && o.VerificationStatus == VerificationStatus.Unverified, cancellationToken);
+    }
+}
