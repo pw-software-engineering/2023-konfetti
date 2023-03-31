65f7338ccf607920c9a9012da4c5d5eb7b33ac59
Michal Boguszewski <michal1842001@gmail.com>
2023-03-31 22:55:49 +0200

    Add tests for DateOnly serialization

--- src/back/TicketManager/tests/Core/TicketManager.Core.ServicesTests/Extensions/JsonConverters/DateOnlyConverterTests.cs
+++ src/back/TicketManager/tests/Core/TicketManager.Core.ServicesTests/Extensions/JsonConverters/DateOnlyConverterTests.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using FluentAssertions;
 using TicketManager.Core.Services.Extensions.JsonConverters;
 using Xunit;
@@ -41,6 +42,44 @@ public class DateOnlyConverterTests
         var result = JsonSerializer.Deserialize<Dto>(json, new JsonSerializerOptions { Converters = { new DateOnlyConverter() } });
 
         result!.Date.Should().Be(date);
+    }
+
+    [Theory]
+    [InlineData(2023, 1, 1)]
+    [InlineData(2000, 12, 31)]
+    [InlineData(1, 1, 1)]
+    [InlineData(3035, 2, 27)]
+    public void WhenConverterIsNotProvider_SerializationShouldThrowException(int year, int month, int day)
+    {
+        var dto = new Dto()
+        {
+            Date = new DateOnly(year, month, day),
+        };
+
+        var func = () => JsonSerializer.Serialize(dto);
+        
+        func.Should().Throw<Exception>();
+    }
+    
+    [Theory]
+    [InlineData(2023, 1, 1)]
+    [InlineData(2000, 12, 31)]
+    [InlineData(1, 1, 1)]
+    [InlineData(3035, 2, 27)]
+    public void WhenConverterIsProvider_SerializationShouldReturnValue(int year, int month, int day)
+    {
+        var dto = new Dto()
+        {
+            Date = new DateOnly(year, month, day),
+        };
+        var expected = GetJson(dto.Date);
+
+        var actual = JsonSerializer.Serialize(dto, new JsonSerializerOptions { Converters = { new DateOnlyConverter() } });
+        
+        var actualWithoutWhiteSpaces = Regex.Replace(actual, "\\s", "");
+        var expectedWithoutWhiteSpaces = Regex.Replace(expected, "\\s", "");
+
+        actualWithoutWhiteSpaces.Should().BeEquivalentTo(expectedWithoutWhiteSpaces);
     }
 
     private string GetJson(DateOnly date)
