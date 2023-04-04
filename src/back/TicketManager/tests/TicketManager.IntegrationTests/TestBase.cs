fa7dc0487f6138702c2e10039ed05af44847c47f
Michal Boguszewski <michal1842001@gmail.com>
2023-04-04 17:02:34 +0200

    Refactor TestBase

--- src/back/TicketManager/tests/TicketManager.IntegrationTests/TestBase.cs
+++ src/back/TicketManager/tests/TicketManager.IntegrationTests/TestBase.cs
@@ -2,10 +2,10 @@ using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using TicketManager.Core.Contracts.Accounts;
+using TicketManager.Core.Contracts.Common;
 using TicketManager.Core.Contracts.Organizers;
 using TicketManager.Core.Contracts.Users;
 using TicketManager.Core.Domain.Accounts;
-using TicketManager.Core.Domain.Organizer;
 using TicketManager.Core.Services.DataAccess;
 using TicketManager.Core.Services.DataAccess.Repositories;
 using TicketManager.Core.Services.Endpoints.Accounts;
@@ -13,7 +13,6 @@ using TicketManager.Core.Services.Endpoints.Organizers;
 using TicketManager.Core.Services.Endpoints.Users;
 using TicketManager.Core.Services.Services.PasswordManagers;
 using TicketManager.IntegrationTests.Extensions;
-using Xunit;
 
 namespace TicketManager.IntegrationTests;
 
@@ -26,8 +25,8 @@ public class TestBase : IAsyncDisposable
     protected readonly HttpClient OrganizerClient;
     protected readonly HttpClient AdminClient;
 
-    protected readonly UserDto DefaultUser;
-    protected readonly OrganizerDto DefaultOrganizer;
+    protected UserDto DefaultUser;
+    protected OrganizerDto DefaultOrganizer;
 
     public TestBase()
     {
@@ -38,53 +37,17 @@ public class TestBase : IAsyncDisposable
         var password = "Password1";
         
         UserClient = app.CreateClient();
-        DefaultUser = new()
-        {
-            Email = "user@user.com",
-            FirstName = "name",
-            LastName = "lastname",
-            BirthDate = new DateOnly(2000, 1, 1),
-        };
-        UserClient.POSTAsync<RegisterUserEndpoint, RegisterUserRequest>(new RegisterUserRequest
-        {
-            Email = DefaultUser.Email,
-            Password = password,
-            FirstName = DefaultUser.FirstName,
-            LastName = DefaultUser.LastName,
-            BirthDate = DefaultUser.BirthDate,
-        }).Wait();
-        var userLoginTask = UserClient.POSTAsync<AccountLoginEndpoint, AccountLoginRequest, AccountLoginResponse>(new AccountLoginRequest
-        {
-            Email = DefaultUser.Email,
-            Password = password,
-        });
-        userLoginTask.Wait();
-        var token = userLoginTask.Result.Result!.AccessToken;
-        UserClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-        var userIdTask = UserClient.GETAsync<UserViewEndpoint, UserViewRequest, UserDto>(new());
-        userIdTask.Wait();
-        DefaultUser.Id = userIdTask.Result.Result!.Id;
+        ConfigureUserAsync(password).Wait();
 
-        AdminClient = app.CreateClient();
-        
-        using var scope = app.Services.CreateScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<CoreDbContext>();
+        AdminClient = app.CreateClient(); ;
+        ConfigureAdminAsync(password).Wait();
 
-        var accounts = new Repository<Account, Guid>(dbContext);
-        var adminAccount = dbContext.Accounts.AsTracking().First(a => a.Role == AccountRoles.Admin);
-        adminAccount.SetPassword(new PasswordManager().GetHash(password));
-        accounts.UpdateAsync(adminAccount, default).Wait();
-
-        var adminLoginTask = AdminClient.PostSuccessAsync<AccountLoginEndpoint, AccountLoginRequest, AccountLoginResponse>(new()
-        {
-            Email = "admin@email.com",
-            Password = password,
-        });
-        adminLoginTask.Wait();
-        token = adminLoginTask.Result.AccessToken;
-        AdminClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-        
         OrganizerClient = app.CreateClient();
+        ConfigureOrganizerAsync(password).Wait();
+    }
+
+    private async Task ConfigureOrganizerAsync(string password)
+    {
         DefaultOrganizer = new()
         {
             Email = "organizer@organizer.com",
@@ -96,7 +59,7 @@ public class TestBase : IAsyncDisposable
             TaxIdType = TaxIdTypeDto.Pesel,
             VerificationStatus = VerificationStatusDto.VerifiedPositively,
         };
-        OrganizerClient.POSTAsync<RegisterOrganizerEndpoint, RegisterOrganizerRequest>(new RegisterOrganizerRequest
+        await OrganizerClient.PostSuccessAsync<RegisterOrganizerEndpoint, RegisterOrganizerRequest>(new RegisterOrganizerRequest
         {
             Email = DefaultOrganizer.Email,
             Password = password,
@@ -106,25 +69,77 @@ public class TestBase : IAsyncDisposable
             PhoneNumber = DefaultOrganizer.PhoneNumber,
             TaxId = DefaultOrganizer.TaxId,
             TaxIdType = TaxIdTypeDto.Pesel,
-        }).Wait();
-        // Authorize organizer to be able to login
-        var repository = new Repository<Organizer, Guid>(dbContext);
+        });
         
-        var dbOrganizer = dbContext.Organizers.AsTracking().First(o => o.Email == DefaultOrganizer.Email);
-        dbOrganizer.Decide(true);
-        repository.UpdateAsync(dbOrganizer, default).Wait();
-
-        var organizerLoginTask = OrganizerClient.POSTAsync<AccountLoginEndpoint, AccountLoginRequest, AccountLoginResponse>(new AccountLoginRequest
+        var organizers = await AdminClient.GetSuccessAsync<OrganizerListEndpoint, OrganizerListRequest, PaginatedResponse<OrganizerDto>>(new()
         {
-            Email = DefaultOrganizer.Email,
+            PageNumber = 0,
+            PageSize = 10,
+        });
+        var organizerToDecide = organizers.Items.First(o => o.Email == DefaultOrganizer.Email);
+        await AdminClient.PostSuccessAsync<OrganizerDecideEndpoint, OrganizerDecideRequest>(new()
+        {
+            OrganizerId = organizerToDecide.Id,
+            IsAccepted = true,
+        });
+
+        var organizerLogin = await OrganizerClient
+            .PostSuccessAsync<AccountLoginEndpoint, AccountLoginRequest, AccountLoginResponse>(new()
+            {
+                Email = DefaultOrganizer.Email,
+                Password = password,
+            });
+        var token = organizerLogin.AccessToken;
+        OrganizerClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+        var organizerId = await OrganizerClient.GetSuccessAsync<OrganizerViewEndpoint, OrganizerViewRequest, OrganizerDto>(new());
+        DefaultOrganizer.Id = organizerId.Id;
+    }
+
+    private async Task ConfigureAdminAsync(string password)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<CoreDbContext>();
+
+        var accounts = new Repository<Account, Guid>(dbContext);
+        var adminAccount = dbContext.Accounts.AsTracking().First(a => a.Role == AccountRoles.Admin);
+        adminAccount.SetPassword(new PasswordManager().GetHash(password));
+        await accounts.UpdateAsync(adminAccount, default);
+
+        var adminLogin = await AdminClient.PostSuccessAsync<AccountLoginEndpoint, AccountLoginRequest, AccountLoginResponse>(new()
+        {
+            Email = "admin@email.com",
             Password = password,
         });
-        organizerLoginTask.Wait();
-        token = organizerLoginTask.Result.Result!.AccessToken;
-        OrganizerClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-        var organizerIdTask = OrganizerClient.GETAsync<OrganizerViewEndpoint, OrganizerViewRequest, OrganizerDto>(new());
-        organizerIdTask.Wait();
-        DefaultOrganizer.Id = organizerIdTask.Result.Result!.Id;
+        var token = adminLogin.AccessToken;
+        AdminClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+    }
+
+    private async Task ConfigureUserAsync(string password)
+    {
+        DefaultUser = new()
+        {
+            Email = "user@user.com",
+            FirstName = "name",
+            LastName = "lastname",
+            BirthDate = new DateOnly(2000, 1, 1),
+        };
+        await UserClient.POSTAsync<RegisterUserEndpoint, RegisterUserRequest>(new RegisterUserRequest
+        {
+            Email = DefaultUser.Email,
+            Password = password,
+            FirstName = DefaultUser.FirstName,
+            LastName = DefaultUser.LastName,
+            BirthDate = DefaultUser.BirthDate,
+        });
+        var userLogin = await UserClient.PostSuccessAsync<AccountLoginEndpoint, AccountLoginRequest, AccountLoginResponse>(new AccountLoginRequest
+        {
+            Email = DefaultUser.Email,
+            Password = password,
+        });
+        var token = userLogin.AccessToken;
+        UserClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+        var userId = await UserClient.GetSuccessAsync<UserViewEndpoint, UserViewRequest, UserDto>(new());
+        DefaultUser.Id = userId.Id;
     }
 
     public async ValueTask DisposeAsync()
