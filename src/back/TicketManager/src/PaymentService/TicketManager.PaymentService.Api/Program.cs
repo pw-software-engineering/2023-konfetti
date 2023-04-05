b602cec2d90d7b9115e2016363630dd899fe7a25
Michal Boguszewski <michal1842001@gmail.com>
2023-04-05 21:21:18 +0200

    Add fast endpoints

--- src/back/TicketManager/src/PaymentService/TicketManager.PaymentService.Api/Program.cs
+++ src/back/TicketManager/src/PaymentService/TicketManager.PaymentService.Api/Program.cs
@@ -1,6 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using TicketManager.PaymentService.Contracts.Validation;
 
-app.MapGet("/", () => "Hello World!");
+namespace TicketManager.PaymentService.Api;
 
-app.Run();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddFastEndpoints();
+        
+        builder.Services.AddSwaggerDoc();
+        
+        var app = builder.Build();
+
+        app.UseAuthorization();
+        app.UseFastEndpoints(c =>
+        {
+            c.Errors.ResponseBuilder = (failures, ctx, statusCode) => new ValidationErrorResponse
+            {
+                Errors = failures.Select(f => new ValidationError
+                    {
+                        ErrorCode = int.Parse(f.ErrorCode),
+                        ErrorMessage = f.ErrorMessage,
+                    })
+                    .ToList(),
+            };
+        });
+        app.UseSwaggerGen();
+
+        app.Run();
+    }
+}
