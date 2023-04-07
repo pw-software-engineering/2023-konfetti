7b67bca5ad395c91300bfcae760fb57dee042a41
Birchman <jakubbrzoskowski@gmail.com>
2023-04-07 11:43:07 +0200

    Add basic input

--- src/back/CrossTeamTestSuite/Program.cs
+++ src/back/CrossTeamTestSuite/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length != 3)
+        {
+            throw new ArgumentException("Wrong number of arguments provided");
+        }
+
+        var address = args[0];
+        var adminEmail = args[1];
+        var adminPassword = args[2];
+        
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Tested app is at {address}");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Admin email is {adminEmail}");
+        Console.WriteLine($"Admin password is {adminPassword}");
     }
 }
 
