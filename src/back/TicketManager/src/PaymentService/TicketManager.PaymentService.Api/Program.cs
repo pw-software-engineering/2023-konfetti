c0e7fc780afa4943de4f62ac2a1451ab632ce961
Michał Boguszewski <67049686+michal12334@users.noreply.github.com>
2023-04-07 12:27:30 +0200

    Create payment app (#61)

---
+++ src/back/TicketManager/src/PaymentService/TicketManager.PaymentService.Api/Program.cs
@@ -0,0 +1,44 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.EntityFrameworkCore;
+using TicketManager.PaymentService.Contracts.Validation;
+using TicketManager.PaymentService.Services.DataAccess;
+
+namespace TicketManager.PaymentService.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<PaymentDbContext>(
+            opts => opts
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .UseInMemoryDatabase("payment-db")
+        );
+        
+        builder.Services.AddFastEndpoints();
+        
+        builder.Services.AddSwaggerDoc();
+        
+        var app = builder.Build();
+
+        app.UseAuthorization();
+        app.UseFastEndpoints(c =>
+        {
+            c.Errors.ResponseBuilder = (failures, ctx, statusCode) => new ValidationErrorResponse
+            {
+                Errors = failures.Select(f => new ValidationError
+                    {
+                        ErrorCode = int.Parse(f.ErrorCode),
+                        ErrorMessage = f.ErrorMessage,
+                    })
+                    .ToList(),
+            };
+        });
+        app.UseSwaggerGen();
+
+        app.Run();
+    }
+}
