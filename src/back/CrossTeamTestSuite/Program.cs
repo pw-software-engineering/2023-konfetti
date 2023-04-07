8a72775b1a4d74089aa0e3712eb4c89faf51d8e7
Jakub Brzóskowski <43820607+jaBrzosko@users.noreply.github.com>
2023-04-07 12:26:10 +0200

    Feature/ctts basic arch (#63)

---
+++ src/back/CrossTeamTestSuite/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CrossTeamTestSuite;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            throw new ArgumentException("Wrong number of arguments provided");
+        }
+
+        var address = args[0];
+        var adminEmail = args[1];
+        var adminPassword = args[2];
+        
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Tested app is at {address}");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Admin email is {adminEmail}");
+        Console.WriteLine($"Admin password is {adminPassword}");
+    }
+}
+
