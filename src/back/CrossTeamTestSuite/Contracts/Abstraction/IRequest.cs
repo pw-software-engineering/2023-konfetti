92b0aaa28c389977c820076b4137a9a5a5a4237c
Michal Boguszewski <michal1842001@gmail.com>
2023-04-09 20:11:40 +0200

    Add JsonIgnore attributes

--- src/back/CrossTeamTestSuite/Contracts/Abstraction/IRequest.cs
+++ src/back/CrossTeamTestSuite/Contracts/Abstraction/IRequest.cs
@@ -1,14 +1,20 @@
+using System.Text.Json.Serialization;
+
 namespace CrossTeamTestSuite.Contracts.Abstraction;
 
 public interface IRequest<TResponse>
     where TResponse : class
 {
+    [JsonIgnore]
     public string Path { get; }
+    [JsonIgnore]
     public RequestType Type { get; }
 }
 
 public interface IRequest
 {
+    [JsonIgnore]
     public string Path { get; }
+    [JsonIgnore]
     public RequestType Type { get; }
 }
