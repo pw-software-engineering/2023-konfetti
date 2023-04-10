5727159bf97b16b6dd4c32c9b1e9b84886e36100
Michal Boguszewski <michal1842001@gmail.com>
2023-04-10 12:03:49 +0200

    Create test for non empty request

--- src/back/CrossTeamTestSuite/tests/CrossTeamTestSuiteTests/Endpoints/Converters/GetQueryParamsConverters/GetQueryParamsConverterTests.cs
+++ src/back/CrossTeamTestSuite/tests/CrossTeamTestSuiteTests/Endpoints/Converters/GetQueryParamsConverters/GetQueryParamsConverterTests.cs
@@ -16,12 +16,35 @@ public class GetQueryParamsConverterTests
         public RequestType Type => RequestType.Get;
     }
     
+    private class Request : IRequest
+    {
+        [JsonIgnore]
+        public string Path => "/path";
+        [JsonIgnore]
+        public RequestType Type => RequestType.Get;
+
+        public int PageSize => 1;
+        public int PageNumber => 12;
+    }
+    
     [Fact]
     public void WhenEmptyRequestIsProvided_ItShouldReturnEmptyQueryParams()
     {
         var expected = "";
         var converter = new GetQueryParamConverter<EmptyRequest>();
         var request = new EmptyRequest();
+
+        var actual = converter.GetParams(request);
+
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void WhenRequestWithManyFieldsIsProvided_ItShouldReturnCorrectQueryParams()
+    {
+        var expected = "?PageSize=1PageNumber=12";
+        var converter = new GetQueryParamConverter<Request>();
+        var request = new Request();
 
         var actual = converter.GetParams(request);
 
