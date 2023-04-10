d1dc900350fb8874e56003c46e09a00a2d176b79
Michal Boguszewski <michal1842001@gmail.com>
2023-04-10 12:28:31 +0200

    Refactor GetQueryParamConverter

--- src/back/CrossTeamTestSuite/src/CrossTeamTestSuite/Endpoints/Converters/GetQueryParamsConverters/GetQueryParamConverter.cs
+++ src/back/CrossTeamTestSuite/src/CrossTeamTestSuite/Endpoints/Converters/GetQueryParamsConverters/GetQueryParamConverter.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text;
 using System.Text.Json.Serialization;
 using CrossTeamTestSuite.Endpoints.Contracts.Abstraction;
@@ -21,40 +22,68 @@ public class GetQueryParamConverter<TRequest>
 
     public string GetParams(TRequest request)
     {
-        var sb = new StringBuilder();
-        var properties = typeof(TRequest)
+        var resultBuilder = new StringBuilder();
+        var properties = GetProperties();
+        foreach (var p in properties)
+        {
+            AppendProperty(request, p, resultBuilder);
+        }
+
+        if (resultBuilder.Length > 0)
+        {
+            resultBuilder.Insert(0, '?');
+        }
+        
+        return resultBuilder.ToString();
+    }
+
+    private void AppendProperty(TRequest request, PropertyInfo p, StringBuilder resultBuilder)
+    {
+        var valueConverter = GetValueConverter(p);
+        var conversionResult = GetConversionResult(request, valueConverter, p);
+        AppendConversionResult(resultBuilder, conversionResult);
+    }
+
+    private static void AppendConversionResult(StringBuilder resultBuilder, object? conversionResult)
+    {
+        if (resultBuilder.Length > 0)
+        {
+            resultBuilder.Append('&');
+        }
+
+        resultBuilder.Append(conversionResult);
+    }
+
+    private static object? GetConversionResult(TRequest request, object valueConverter, PropertyInfo p)
+    {
+        var convertMethod = valueConverter.GetType().GetMethods().First(m => m.Name == "Convert");
+        var conversionResult = convertMethod.Invoke(valueConverter, new[] { p.Name, p.GetValue(request) });
+        return conversionResult;
+    }
+
+    private object GetValueConverter(PropertyInfo p)
+    {
+        var valueConverter = valueConverters
+            .FirstOrDefault(vc => vc
+                .GetType()
+                .GetMethods()
+                .First(m => m.Name == "Convert")
+                .GetParameters()
+                .Any(mp => mp.ParameterType == p.PropertyType));
+
+        if (valueConverter is null)
+        {
+            throw new UnsupportedTypeException();
+        }
+
+        return valueConverter;
+    }
+
+    private static List<PropertyInfo> GetProperties()
+    {
+        return typeof(TRequest)
             .GetProperties()
             .Where(p => p.GetCustomAttributes(typeof(JsonIgnoreAttribute), true).FirstOrDefault() is null)
             .ToList();
-        foreach (var p in properties)
-        {
-            var valueConverter = valueConverters
-                .FirstOrDefault(vc => vc
-                    .GetType()
-                    .GetMethods()
-                    .First(m => m.Name == "Convert")
-                    .GetParameters()
-                    .Any(mp => mp.ParameterType == p.PropertyType));
-
-            if (valueConverter is null)
-            {
-                throw new UnsupportedTypeException();
-            }
-
-            var convertMethod = valueConverter.GetType().GetMethods().First(m => m.Name == "Convert");
-            var conversionResult = convertMethod.Invoke(valueConverter, new[] { p.Name, p.GetValue(request) });
-            if (sb.Length > 0)
-            {
-                sb.Append('&');
-            }
-            sb.Append(conversionResult);
-        }
-
-        if (sb.Length > 0)
-        {
-            sb.Insert(0, '?');
-        }
-        
-        return sb.ToString();
     }
 }
