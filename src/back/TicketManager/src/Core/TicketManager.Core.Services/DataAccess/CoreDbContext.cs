0693be6b5989e5d9588054c6fc8a1209f6d8ab50
Birchman <jakubbrzoskowski@gmail.com>
2023-04-12 18:36:34 +0200

    Add default value for RowVersion

--- src/back/TicketManager/src/Core/TicketManager.Core.Services/DataAccess/CoreDbContext.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Services/DataAccess/CoreDbContext.cs
@@ -126,7 +126,8 @@ public static class ModelBuilderExtensions
         cfg.Property<byte[]>("RowVersion")
             .HasColumnName("RowVersion")
             .IsRowVersion()
-            .IsRequired();
+            .IsRequired()
+            .HasDefaultValue(Array.Empty<byte>());
         cfg.Property(e => e.DateModified).IsConcurrencyToken();
     }
 }
