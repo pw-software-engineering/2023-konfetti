69a05c3460a75b8eb940e59718a24c514611cc70
Birchman <jakubbrzoskowski@gmail.com>
2023-04-17 19:43:26 +0200

    Change class Id type

--- src/back/TicketManager/src/Core/TicketManager.Core.Domain/Events/SeatReservation.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Domain/Events/SeatReservation.cs
@@ -1,6 +1,6 @@
 namespace TicketManager.Core.Domain.Events;
 
-public record SeatReservation((Guid EventId, string SectorName) SectorId, Guid UserId, int ReservedSeatNumber)
+public record SeatReservation(Guid SectorId, Guid UserId, int ReservedSeatNumber)
 {
     public Guid Id = Guid.NewGuid();
     public DateTime CreationDate = DateTime.UtcNow;
--- src/back/TicketManager/src/Core/TicketManager.Core.Domain/Events/Sector.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Domain/Events/Sector.cs
@@ -3,11 +3,11 @@ using TicketManager.Core.Domain.Common;
 namespace TicketManager.Core.Domain.Events;
 
 public class Sector: 
-    IAggregateRoot<(Guid EventId, string Name)>, IOptimisticConcurrent
+    IAggregateRoot<Guid>, IOptimisticConcurrent
 {
-    public (Guid EventId, string Name) Id => (EventId, Name);
+    public Guid Id { get; private init; }
     public Guid EventId { get; private init; }
-    public string Name { get; private init; } = null!;
+    public string Name { get; private set; } = null!;
     public int PriceInSmallestUnit { get; private set; }
     public int NumberOfColumns { get; private set; }
     public int NumberOfRows { get; private set; }
