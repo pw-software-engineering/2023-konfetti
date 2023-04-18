fbce6b289b762daf4981c3cd1dd805fee514863d
Birchman <jakubbrzoskowski@gmail.com>
2023-04-18 15:00:00 +0200

    Change accessors

--- src/back/TicketManager/src/Core/TicketManager.Core.Domain/Events/SeatReservation.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Domain/Events/SeatReservation.cs
@@ -2,6 +2,6 @@ namespace TicketManager.Core.Domain.Events;
 
 public record SeatReservation(Guid SectorId, Guid UserId, int ReservedSeatNumber)
 {
-    public Guid Id = Guid.NewGuid();
-    public DateTime CreationDate = DateTime.UtcNow;
+    public Guid Id { get; private init; }= Guid.NewGuid();
+    public DateTime CreationDate { get; private init; }= DateTime.UtcNow;
 }
