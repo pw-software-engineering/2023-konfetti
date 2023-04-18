4dcb25fc88e8bd3a18bed9e76c93b1663aff38d5
Birchman <jakubbrzoskowski@gmail.com>
2023-04-18 22:23:34 +0200

    Add running admin test

--- src/back/CrossTeamTestSuite/src/CrossTeamTestSuite/Program.cs
+++ src/back/CrossTeamTestSuite/src/CrossTeamTestSuite/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.Text.Json;
+using CrossTeamTestSuite.Data;
 using CrossTeamTestSuite.Endpoints.Contracts.Accounts;
 using CrossTeamTestSuite.Endpoints.Contracts.Common;
 using CrossTeamTestSuite.Endpoints.Contracts.Organizers;
 using CrossTeamTestSuite.Endpoints.Extensions;
+using CrossTeamTestSuite.Tests.Admin;
 using CrossTeamTestSuite.Tests.Examples;
 using CrossTeamTestSuite.TestsInfrastructure;
 
@@ -10,7 +12,7 @@ namespace CrossTeamTestSuite;
 
 class Program
 {
-    public async static Task Main(string[] args)
+    public static async Task Main(string[] args)
     {
         if (args.Length != 3)
         {
@@ -20,36 +22,10 @@ class Program
         var address = args[0];
         var adminEmail = args[1];
         var adminPassword = args[2];
-        
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine($"Tested app is at {address}");
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Admin email is {adminEmail}");
-        Console.WriteLine($"Admin password is {adminPassword}");
-
-        Console.ForegroundColor = ConsoleColor.White;
-        var httpClient = new HttpClient();
-        httpClient.BaseAddress = new Uri(address);
-        var adminLogin = await httpClient.CallEndpointAsync<AccountLoginRequest, AccountLoginResponse>(new AccountLoginRequest()
-        {
-            Email = adminEmail,
-            Password = adminPassword,
-        });
-        var json = JsonSerializer.Serialize(adminLogin);
-        Console.WriteLine(json);
-        httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {adminLogin!.AccessToken}");
-        
-        var organizers = await httpClient.CallEndpointAsync<OrganizerListRequest, PaginatedResponse<OrganizerDto>>(new OrganizerListRequest()
-        {
-            PageNumber = 0,
-            PageSize = 10,
-        });
-        json = JsonSerializer.Serialize(organizers);
-        Console.WriteLine(json);
+        ApiClientSingleton.ConfigureClient(address);
 
         await new TestPipeline()
-            .AddMultiTest(new SumTest())
-            .AddTest(new MultiplyTest())
+            .AddTest(new AdminLoginTest(adminEmail, adminPassword))
             .GetExecutor()
             .ExecuteAsync();
     }
