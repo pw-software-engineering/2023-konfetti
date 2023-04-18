7c844234d9d25dd73eb9a6bd37fd58524b36a549
Birchman <jakubbrzoskowski@gmail.com>
2023-04-18 22:22:28 +0200

    Add endpoint instance basic architecture

---
+++ src/back/CrossTeamTestSuite/src/CrossTeamTestSuite/Endpoints/Instances/AccountLoginInstance.cs
@@ -0,0 +1,14 @@
+using CrossTeamTestSuite.Endpoints.Contracts.Accounts;
+using CrossTeamTestSuite.Endpoints.Extensions;
+
+namespace CrossTeamTestSuite.Endpoints.Instances;
+
+public class AccountLoginInstance: EndpointInstance<AccountLoginRequest, AccountLoginResponse>
+{
+
+    public override async Task<AccountLoginResponse> HandleEndpointAsync(AccountLoginRequest request)
+    {
+        var token =  await HttpClient.CallEndpointAsync<AccountLoginRequest, AccountLoginResponse>(request);
+        return token!;
+    }
+}
