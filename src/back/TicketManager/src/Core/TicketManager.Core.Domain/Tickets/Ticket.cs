b9c51d50e61655c2b30bc49fb4bc02b62d0890a6
Michal Boguszewski <michal1842001@gmail.com>
2023-04-20 22:13:09 +0200

    Create Ticket

---
+++ src/back/TicketManager/src/Core/TicketManager.Core.Domain/Tickets/Ticket.cs
@@ -0,0 +1,25 @@
+using TicketManager.Core.Domain.Common;
+
+namespace TicketManager.Core.Domain.Tickets;
+
+public class Ticket : IAggregateRoot<Guid>, IOptimisticConcurrent
+{
+    private readonly List<TicketSeat> seats = new();
+
+    public Guid Id { get; private init; }
+    public Guid EventId { get; private init; }
+    public Guid SectorId { get; private init; }
+    public IReadOnlyList<TicketSeat> Seats => seats;
+
+    public DateTime DateModified { get; set; }
+    
+    private Ticket() 
+    { }
+    
+    public Ticket(Guid eventId, Guid sectorId, IEnumerable<TicketSeat> seats)
+    {
+        EventId = eventId;
+        SectorId = sectorId;
+        this.seats.AddRange(seats);
+    }
+}
