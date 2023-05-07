7bfd981b1a55ba512965189a98dc2015cc2015d8
jaBrzosko <jakubbrzoskowski@gmail.com>
2023-05-07 17:36:48 +0200

    Add role checks

--- src/back/TicketManager/tests/Core/TicketManager.Core.ServicesTests/Endpoints/Roles/RoleTests/EventRoleTests.cs
+++ src/back/TicketManager/tests/Core/TicketManager.Core.ServicesTests/Endpoints/Roles/RoleTests/EventRoleTests.cs
@@ -27,4 +27,43 @@ public class EventRoleTests
         var testInstance = testsBase.GetRoleTestInstance<ListEventsForOrganizerEndpoint>();
         testInstance.AsOrganizer().Check();
     }
+    
+    [Fact]
+    public void WhenEventHoldChecked_ItShouldAllowOrganizerAndAdmin()
+    {
+        var testInstance = testsBase.GetRoleTestInstance<EventHoldEndpoint>();
+        testInstance.AsOrganizer().AsAdmin().Check();
+    }
+    [Fact]
+    public void WhenEventPublishChecked_ItShouldAllowOrganizerAndAdmin()
+    {
+        var testInstance = testsBase.GetRoleTestInstance<EventPublishEndpoint>();
+        testInstance.AsOrganizer().AsAdmin().Check();
+    }
+    [Fact]
+    public void WhenEventRecallChecked_ItShouldAllowOrganizerAndAdmin()
+    {
+        var testInstance = testsBase.GetRoleTestInstance<EventRecallEndpoint>();
+        testInstance.AsOrganizer().AsAdmin().Check();
+    }
+    
+    [Fact]
+    public void WhenEventRestartChecked_ItShouldAllowOrganizerAndAdmin()
+    {
+        var testInstance = testsBase.GetRoleTestInstance<EventRestartEndpoint>();
+        testInstance.AsOrganizer().AsAdmin().Check();
+    }
+
+    [Fact]
+    public void WhenEventSaleStartChecked_ItShouldAllowOnlyOrganizer()
+    {
+        var testInstance = testsBase.GetRoleTestInstance<EventSaleStartEndpoint>();
+        testInstance.AsOrganizer().Check();
+    }
+    [Fact]
+    public void WhenEventSaleStopChecked_ItShouldAllowOnlyOrganizer()
+    {
+        var testInstance = testsBase.GetRoleTestInstance<EventSaleStopEndpoint>();
+        testInstance.AsOrganizer().Check();
+    }
 }
