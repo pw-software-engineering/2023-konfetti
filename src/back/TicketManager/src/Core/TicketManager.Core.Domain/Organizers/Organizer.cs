bf92b382d5edbbb1bf8b247810f40b60b0e099aa
Michał Boguszewski <67049686+michal12334@users.noreply.github.com>
2023-05-13 21:19:28 +0200

    Edit organizer profile (#97)

--- src/back/TicketManager/src/Core/TicketManager.Core.Domain/Organizers/Organizer.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Domain/Organizers/Organizer.cs
@@ -6,13 +6,13 @@ namespace TicketManager.Core.Domain.Organizer;
 public class Organizer : IAggregateRoot<Guid>, IAccount, IOptimisticConcurrent
 {
     public Guid Id { get; private init; }
-    public string Email { get; private init; } = null!;
-    public string CompanyName { get; private init; } = null!;
-    public string Address { get; private init; } = null!;
-    public string TaxId { get; private init; } = null!;
-    public TaxIdType TaxIdType { get; private init; }
-    public string DisplayName { get; private init; } = null!;
-    public string PhoneNumber { get; private init; } = null!;
+    public string Email { get; private set; } = null!;
+    public string CompanyName { get; private set; } = null!;
+    public string Address { get; private set; } = null!;
+    public string TaxId { get; private set; } = null!;
+    public TaxIdType TaxIdType { get; private set; }
+    public string DisplayName { get; private set; } = null!;
+    public string PhoneNumber { get; private set; } = null!;
     public VerificationStatus VerificationStatus { get; private set; }
     public DateTime DateModified { get; set; }
 
@@ -48,7 +48,24 @@ public class Organizer : IAggregateRoot<Guid>, IAccount, IOptimisticConcurrent
     {
         return VerificationStatus == VerificationStatus.VerifiedPositively;
     }
-    
+
+    public void Update(
+        string email,
+        string companyName,
+        string address,
+        string taxId,
+        TaxIdType taxIdType,
+        string displayName,
+        string phoneNumber)
+    {
+        Email = email;
+        CompanyName = companyName;
+        Address = address;
+        TaxId = taxId;
+        TaxIdType = taxIdType;
+        DisplayName = displayName;
+        PhoneNumber = phoneNumber;
+    }
 }
 
 public enum TaxIdType
---
+++ src/back/TicketManager/src/Core/TicketManager.Core.Services/Endpoints/Organizers/UpdateOrganizerEndpoint.cs
@@ -0,0 +1,41 @@
+using FastEndpoints;
+using TicketManager.Core.Contracts.Organizers;
+using TicketManager.Core.Domain.Accounts;
+using TicketManager.Core.Domain.Organizer;
+using TicketManager.Core.Services.DataAccess.Repositories;
+
+namespace TicketManager.Core.Services.Endpoints.Organizers;
+
+public class UpdateOrganizerEndpoint : Endpoint<UpdateOrganizerRequest>
+{
+    private readonly Repository<Organizer, Guid> organizers;
+
+    public UpdateOrganizerEndpoint(Repository<Organizer, Guid> organizers)
+    {
+        this.organizers = organizers;
+    }
+
+    public override void Configure()
+    {
+        Post("/organizer/update");
+        Roles(AccountRoles.Organizer);
+    }
+
+    public override async Task HandleAsync(UpdateOrganizerRequest req, CancellationToken ct)
+    {
+        var organizer = await organizers.FindAndEnsureExistenceAsync(req.AccountId, ct);
+
+        organizer.Update(
+            req.Email ?? organizer.Email,
+            req.CompanyName ?? organizer.CompanyName,
+            req.Address ?? organizer.Address,
+            req.TaxId ?? organizer.TaxId,
+            (TaxIdType?)req.TaxIdType ?? organizer.TaxIdType,
+            req.DisplayName ?? organizer.DisplayName,
+            req.PhoneNumber ?? organizer.PhoneNumber);
+
+        await organizers.UpdateAsync(organizer, ct);
+
+        await SendOkAsync(ct);
+    }
+}
