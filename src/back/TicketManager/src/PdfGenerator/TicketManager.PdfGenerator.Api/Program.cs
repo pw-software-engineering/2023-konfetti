3e8f2f49464ba3549c40a983ec27ec7fa34ba4d7
Michal Boguszewski <michal1842001@gmail.com>
2023-05-13 13:33:26 +0200

    Add masstransit

--- src/back/TicketManager/src/PdfGenerator/TicketManager.PdfGenerator.Api/Program.cs
+++ src/back/TicketManager/src/PdfGenerator/TicketManager.PdfGenerator.Api/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+
 namespace TicketManager.PdfGenerator.Api;
 
 public class Program
@@ -5,6 +7,20 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddMassTransit(x =>
+        {
+            x.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(builder.Configuration["RabbitMqHost"], builder.Configuration["RabbitMqVirtualHost"], cfg =>
+                {
+                    cfg.Username(builder.Configuration["RabbitMqUsername"]);
+                    cfg.Password(builder.Configuration["RabbitMqPassword"]);
+                });
+                cfg.ConfigureEndpoints(ctx);
+            });
+        });
+        
         var app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
