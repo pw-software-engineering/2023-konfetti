948e59dcd81fecc013170419b2dc742ae07d6f7d
jaBrzosko <jakubbrzoskowski@gmail.com>
2023-05-13 18:18:44 +0200

    Add start for integration tests - finish later

--- src/back/TicketManager/tests/TicketManager.IntegrationTests/Events/ManagingEventTests.cs
+++ src/back/TicketManager/tests/TicketManager.IntegrationTests/Events/ManagingEventTests.cs
@@ -1,5 +1,6 @@
 using TicketManager.Core.Contracts.Common;
 using TicketManager.Core.Contracts.Events;
+using TicketManager.Core.Domain.Events;
 using TicketManager.Core.Services.Endpoints.Events;
 using TicketManager.IntegrationTests.Extensions;
 using Xunit;
@@ -8,27 +9,46 @@ namespace TicketManager.IntegrationTests.Events;
 
 public class ManagingEventTests : TestBase
 {
+    private Event defaultEvent;
+    private List<Sector> sectors;
+    private Guid realEventId;
+    public ManagingEventTests()
+    {
+        defaultEvent = new Event(DefaultOrganizer.Id, "event", "description", "location", DateTime.UtcNow.AddDays(1));
+        sectors = new List<Sector>
+        {
+            new Sector(defaultEvent.Id,"sector1", 2, 2, 4),
+            new Sector(defaultEvent.Id,"sector2", 2, 2, 4),
+        };
+    }
     [Fact]
     public async Task Organizer_can_manage_event()
     {
+        await CreateEvent();
+    }
+
+    private async Task VerifyEvent()
+    {
+        //TODO: finish this test
+    }
+
+    private async Task CreateEvent()
+    {
         var eventId = await OrganizerClient.PostSuccessAsync<CreateEventEndpoint, CreateEventRequest, IdResponse>(new()
         {
-            Name = "event",
-            Description = "description",
-            Location = "location",
-            Date = DateTime.UtcNow.AddDays(1),
-            Sectors = new()
+            Name = defaultEvent.Name,
+            Description = defaultEvent.Description,
+            Location = defaultEvent.Location,
+            Date = defaultEvent.Date,
+            Sectors = sectors.Select(s => new SectorDto
             {
-                new()
-                {
-                    Name = "sector1",
-                    PriceInSmallestUnit = 2,
-                    NumberOfRows = 2,
-                    NumberOfColumns = 4,
-                },
-            },
+                Name = s.Name,
+                PriceInSmallestUnit = s.PriceInSmallestUnit,
+                NumberOfColumns = s.NumberOfColumns,
+                NumberOfRows = s.NumberOfRows,
+            }).ToList()
         });
         
-        // TODO: get event with some request and check data correctness
+        realEventId = eventId.Id;
     }
 }
