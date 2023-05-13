ced2c5415cbcbdc1bf7141bbaf9d5f9b682c4c17
Michal Boguszewski <michal1842001@gmail.com>
2023-05-13 13:57:31 +0200

    Add logging

--- src/back/TicketManager/src/PdfGenerator/TicketManager.PdfGenerator.Api/Processes/GenerateTicketPdfConsumer.cs
+++ src/back/TicketManager/src/PdfGenerator/TicketManager.PdfGenerator.Api/Processes/GenerateTicketPdfConsumer.cs
@@ -5,8 +5,16 @@ namespace TicketManager.PdfGenerator.Api.Processes;
 
 public class GenerateTicketPdfConsumer : IConsumer<GenerateTicketPdf>
 {
+    private readonly ILogger<GenerateTicketPdfConsumer> logger;
+
+    public GenerateTicketPdfConsumer(ILogger<GenerateTicketPdfConsumer> logger)
+    {
+        this.logger = logger;
+    }
+
     public Task Consume(ConsumeContext<GenerateTicketPdf> context)
     {
+        logger.LogInformation("Ticket {TicketId} pdf generation started", Guid.NewGuid());
         return Task.CompletedTask;
     }
 }
