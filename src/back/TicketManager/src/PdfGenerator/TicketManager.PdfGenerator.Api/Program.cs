901e3fcb5b4b0b05d352db0a9278b29be9302c49
Michal Boguszewski <michal1842001@gmail.com>
2023-05-13 13:30:23 +0200

    Create pdf generation app

---
+++ src/back/TicketManager/src/PdfGenerator/TicketManager.PdfGenerator.Api/Program.cs
@@ -0,0 +1,14 @@
+namespace TicketManager.PdfGenerator.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
