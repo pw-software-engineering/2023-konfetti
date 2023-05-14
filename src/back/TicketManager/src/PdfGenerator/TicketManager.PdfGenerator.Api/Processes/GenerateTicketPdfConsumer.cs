6137fe1502b7f4242cc5301b03a24f2087f49ea4
jaBrzosko <jakubbrzoskowski@gmail.com>
2023-05-14 14:16:31 +0200

    Add basic pdf generation

--- src/back/TicketManager/src/PdfGenerator/TicketManager.PdfGenerator.Api/Processes/GenerateTicketPdfConsumer.cs
+++ src/back/TicketManager/src/PdfGenerator/TicketManager.PdfGenerator.Api/Processes/GenerateTicketPdfConsumer.cs
@@ -1,3 +1,8 @@
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
 using MassTransit;
 using TicketManager.PdfGenerator.Contracts;
 
@@ -15,6 +20,21 @@ public class GenerateTicketPdfConsumer : IConsumer<GenerateTicketPdf>
     public Task Consume(ConsumeContext<GenerateTicketPdf> context)
     {
         logger.LogInformation("Ticket {TicketId} pdf generation started", Guid.NewGuid());
+
+        PdfWriter writer = new PdfWriter("./demo.pdf");
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+        Paragraph header = new Paragraph("HEADER")
+            .SetTextAlignment(TextAlignment.CENTER)
+            .SetFontSize(20);
+        Paragraph body = new Paragraph("Some super ticket")
+            .SetTextAlignment(TextAlignment.LEFT)
+            .SetFontSize(14);
+
+        document.Add(header);
+        document.Add(body);
+        document.Close();
+        
         return Task.CompletedTask;
     }
 }
