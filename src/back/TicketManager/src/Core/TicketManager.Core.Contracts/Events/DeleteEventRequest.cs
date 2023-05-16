ab202edb5c952598a5e84a7d08fbab5f483f1859
Michal Boguszewski <michal1842001@gmail.com>
2023-05-16 16:29:57 +0200

    Create delete event validator

--- src/back/TicketManager/src/Core/TicketManager.Core.Contracts/Events/DeleteEventRequest.cs
+++ src/back/TicketManager/src/Core/TicketManager.Core.Contracts/Events/DeleteEventRequest.cs
@@ -3,4 +3,9 @@ namespace TicketManager.Core.Contracts.Events;
 public class DeleteEventRequest
 {
     public Guid Id { get; set; }
+
+    public static class ErrorCodes
+    {
+        public const int EventDoesNotExist = 1;
+    }
 }
