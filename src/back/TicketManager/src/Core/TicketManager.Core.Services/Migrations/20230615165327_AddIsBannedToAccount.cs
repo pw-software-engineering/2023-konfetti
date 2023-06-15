4496b189ac93ef3ed1f274c56bde82bbd94194db
Michal Boguszewski <michal1842001@gmail.com>
2023-06-15 18:54:33 +0200

    Do migration

---
+++ src/back/TicketManager/src/Core/TicketManager.Core.Services/Migrations/20230615165327_AddIsBannedToAccount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketManager.Core.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsBannedToAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBanned",
+                table: "Accounts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBanned",
+                table: "Accounts");
+        }
+    }
+}
